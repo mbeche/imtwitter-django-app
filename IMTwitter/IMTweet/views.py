2fe6bc16ea0e37bc8435bb87f321261cbe93e65d
Mike <mb.johnsonbece@gmail.com>
2018-04-28 12:08:41 -0500

    Create landing page for tweets with temporary text

--- IMTwitter/IMTweet/views.py
+++ IMTwitter/IMTweet/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def index(request):
+    return HttpResponse("Hello, world. This is where you will soon be able to interact with a post interface.")
 # Create your views here.
