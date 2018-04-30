68a850446ad3ae7e280804da4b0b3fd9d035c621
Mike <mb.johnsonbece@gmail.com>
2018-04-30 17:11:33 -0500

    Posts loading on the dashboard

--- IMTwitter/IMTweet/urls.py
+++ IMTwitter/IMTweet/urls.py
@@ -4,5 +4,7 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('', views.index, name='index'),
+    path('dashboard/', views.dashboard , name='dashboard'),
+    # path('dashboard/', views.post_list , name='post_list'),
+    # path('', views.index, name='index'),
 ]
--- IMTwitter/IMTweet/views.py
+++ IMTwitter/IMTweet/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .models import Post
+from .models import Comment
 
-def index(request):
-    return HttpResponse("Hello, world. This is where you will soon be able to interact with a post interface.")
+# def index(request):
+#     return HttpResponse("Hello, world. This is where you will soon be able to interact with a post interface.")
 # Create your views here.
+
+@login_required
+def dashboard(request):
+    posts = Post.objects.all()
+    comments = Comment.objects.all()
+    return render(request, 'dashboard.html', {'section': 'dashboard', 'posts':posts, 'comments':comments})
+
+def post_list(request):
+    return render(request, 'post_list.html', {})
