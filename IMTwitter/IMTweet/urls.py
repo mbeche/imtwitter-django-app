a1017419e0726627de68b3520df2bb082ae9572f
Mike <mb.johnsonbece@gmail.com>
2018-05-02 16:29:33 -0500

    Delete post functional. This and edit post now restricted to the user that created the post.

--- IMTwitter/IMTweet/urls.py
+++ IMTwitter/IMTweet/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('addpost/', views.add_post, name='add_post'),
     re_path(r'^post_comment_on_(?P<pk>\d+)/?', views.add_comment_to_post , name='add_comment_to_post'),
     re_path(r'^edit-post/(?P<pk>\d+)/$', views.edit_post, name='edit_post'),
-    # re_path(r'^edit-comment/(?P<pk>\d+)/$', views.edit_comment, name='edit_comment'),
+    re_path(r'^edit-comment/(?P<pk>\d+)/$', views.edit_comment, name='edit_comment'),
+    re_path(r'^delete-post/(?P<pk>\d+)/$', views.delete_post, name='delete_post'),
 ]
--- IMTwitter/IMTweet/views.py
+++ IMTwitter/IMTweet/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
+from django.template.response import TemplateResponse
 from django.contrib.auth.decorators import login_required
 from .models import Post
 from .models import Comment
@@ -48,20 +49,56 @@ def add_comment_to_post(request, pk):
             form = CommentForm()
             return render(request, 'add_comment_to_post.html', {'form': form, 'post':post})
 
+def user_author_check(author, user):
+    return author == user
+
 @login_required
 def edit_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            form.save()
+    user_auth = user_author_check(post.user, request.user)
+    if user_author_check(post.user, request.user):
+        if request.method == "POST":
+            form = PostForm(request.POST, instance=post)
+            if form.is_valid():
+                form.save()
+                return redirect('dashboard')
+        else:
+            form = PostForm(instance=post)
+        return render(request, 'add_post.html', {'form': form, 'post': post, 'user_auth': user_auth})
     else:
-        form = PostForm(instance=post)
-    return render(request, 'add_post.html', {'form': form, 'post': post})
+        return HttpResponse("You do not have permission to edit this post.")
 
+@login_required
+def delete_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if user_author_check(post.user, request.user):
+        if request.method == "POST":
+            form = PostForm(request.POST, instance=post)
+            post.delete()
+            return redirect('dashboard')
+        else:
+            form = PostForm(instance=post)
+        return render(request, 'add_post.html', {'form': form, 'post': post})
+    else:
+        return HttpResponse("You do not have permission to delete this post.")
 
 # @login_required
-# def edit_comment(request, pk):
+# def delete_comment(request, pk):
+#     comment = get_object_or_404(Comment, pk=pk)
+#     if user_author_check(comment.user, request.user):
+#         if request.method == "POST":
+#             form = CommentForm(request.POST, instance=comment)
+#             post.delete()
+#             return redirect('dashboard')
+#         else:
+#             form = PostForm(instance=post)
+#         return render(request, 'add_post.html', {'form': form, 'post': post})
+#     else:
+#         return HttpResponse("You do not have permission to delete this post.")
+
+@login_required
+def edit_comment(request, pk):
+    return
 #     post = get_object_or_404(Post, pk=pk)
 #     if request.method == "POST":
 #         form = CommentForm(request.POST)
