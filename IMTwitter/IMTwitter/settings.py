12f6bdf43d566ac12b918538a6ccd7c6d7f110c0
Mike <mb.johnsonbece@gmail.com>
2018-05-05 09:26:26 -0500

    Change look to remove features not up and running yet

--- IMTwitter/IMTwitter/settings.py
+++ IMTwitter/IMTwitter/settings.py
@@ -74,6 +74,8 @@ LOGOUT_REDIRECT_URL = 'home'
 
 WSGI_APPLICATION = 'imtwitter.wsgi.application'
 
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
