7dd5dacc83d4140c735771d163bd6ff845cb737a
Mike <mb.johnsonbece@gmail.com>
2018-05-05 22:30:02 -0500

    Trying to get all folders lowercase

---
+++ imtwitter/imtwitter2/wsgi.py
@@ -0,0 +1,16 @@
+"""imt
+WSGI config for imtwitter project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "imtwitter.settings")
+
+application = get_wsgi_application()
